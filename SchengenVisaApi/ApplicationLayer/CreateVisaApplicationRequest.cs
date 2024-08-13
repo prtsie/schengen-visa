cc7eb1d1688b84d05a2e7c457e1a190336eaee70
prtsie <yonhsup@mail.ru>
2024-08-13 17:52:45 +0300

    Added application layer

---
+++ SchengenVisaApi/ApplicationLayer/CreateVisaApplicationRequest.cs
@@ -0,0 +1,30 @@
+﻿using Domains.ApplicantDomain;
+using Domains.VisaApplicationDomain;
+
+namespace ApplicationLayer;
+
+public record CreateVisaApplicationRequest(
+    Name FullName,
+    Passport Passport,
+    DateOnly BirthDate,
+    string BirthCity,
+    string BirthCountry,
+    string CitizenShip,
+    string CitizenshipByBirth,
+    Gender Gender,
+    MaritalStatus MaritalStatus,
+    Name FatherName,
+    Name MotherName,
+    bool IsNonResident,
+    ReentryPermit ReentryPermit,
+    string JobTitle,
+    PlaceOfWork PlaceOfWork,
+    string DestinationCountry,
+    VisaCategory VisaCategory,
+    bool IsForGroup,
+    RequestedNumberOfEntries RequestedNumberOfEntries,
+    int ValidDaysRequested,
+    PastVisa[] PastVisas,
+    PermissionToDestCountry? PermissionToDestCountry,
+    PastVisit[] PastVisits
+    );
