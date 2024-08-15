54e31d41fc8963ded477dad5505f4c6105832e8d
prtsie <yonhsup@mail.ru>
2024-08-15 18:54:26 +0300

    fixed warnings

--- SchengenVisaApi/ApplicationLayer/Common/IGenericRepository.cs
+++ SchengenVisaApi/ApplicationLayer/Common/IGenericRepository.cs
@@ -9,20 +9,24 @@ namespace ApplicationLayer.Common;
 public interface IGenericRepository<T> where T : class, IEntity
 {
     /// Get all entities from data storage
+    /// <param name="cancellationToken">Cancellation token</param>
     Task<List<T>> GetAllAsync(CancellationToken cancellationToken);
 
     /// Get one entity with specific id
     /// <param name="id">Identifier of entity</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     Task<T> GetOneAsync(Guid id, CancellationToken cancellationToken);
 
     /// Add entity to storage
     /// <param name="entity">Entity to add</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     Task AddAsync(T entity, CancellationToken cancellationToken);
 
     /// <summary>
     /// Update entity in storage
     /// </summary>
     /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Cancellation token</param>
     Task UpdateAsync(T entity, CancellationToken cancellationToken);
 
     /// <summary>
