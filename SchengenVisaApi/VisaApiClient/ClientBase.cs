96acf03acf3c76b9e553098f2ec6f913a83f1b25
prtsie <yonhsup@mail.ru>
2024-08-28 12:46:58 +0300

    Added client library

---
+++ SchengenVisaApi/VisaApiClient/ClientBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace VisaApiClient
+{
+    public class ClientBase
+    {
+        private const string LoginPath = "users/login";
+
+        protected string? AuthToken { get; private set; }
+
+        protected void SetAuthToken(string token)
+        {
+            AuthToken = token;
+        }
+
+        protected Task<HttpRequestMessage> CreateHttpRequestMessageAsync(CancellationToken cancellationToken)
+        {
+            var msg = new HttpRequestMessage();
+            msg.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthToken);
+            return Task.FromResult(msg);
+        }
+
+
+        protected async Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, string url, CancellationToken cancellationToken)
+            => await Task.CompletedTask;
+
+        protected async Task PrepareRequestAsync(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder, CancellationToken cancellationToken)
+            => await Task.CompletedTask;
+
+        protected async Task ProcessResponseAsync(HttpClient client, HttpResponseMessage response, CancellationToken cancellationToken)
+        {
+            if (response.RequestMessage!.RequestUri!.AbsolutePath == LoginPath)
+            {
+                var token = await response.Content.ReadAsStringAsync(cancellationToken);
+                SetAuthToken(token);
+            }
+        }
+    }
+}
